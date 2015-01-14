77c449e832950cef542c96a7ff562c2312a58856
Shawn <shawn.p.wallis@gmail.com>
2015-01-13 20:29:43 -0500

    calling works, but is returning error

--- audible.py
+++ audible.py
@@ -1,6 +1,9 @@
-
+#!c:/Python/python.exe
 import os
-import ctypes
+from ctypes import *
+from ctypes import wintypes
+from UserDict import UserDict
+
 
 DEFAULT_DLL_DIRECTORY = "C:\\Program Files (x86)\\Audible\Bin\\AAXSDKWin.dll"
 DEFAULT_DLL_FILENAME = "AAXSDKWin.dll"
@@ -18,7 +21,42 @@ class AudibleDll(object):
 		(self._parentDir, self._filename) = os.path.split(filepath)
 		os.environ['PATH'] = ';'.join([os.environ['PATH'], self._parentDir])
 		
-		self._handle = ctypes.CDLL(self._filename)
-		self.AAXOpenFileWinW = self._handle['AAXOpenFileWinW']
+		self._handle = CDLL(self._filename)
+		
+		self._loadDllFunctions()
+	
+	@staticmethod
+	def _checkFuncResult(returnCode, funcName, raiseError=False):
+		if returnCode != 0:
+			msg = '%s return code was %d' % (funcName, returnCode)
+			if raiseError is True:
+				raise Exception(msg)
+			else:
+				msg = 'WARNING: ' + msg
+				print msg
+	
+	def _loadDllFunctions(self):
+		self._handle_funcs = UserDict()
+		
+		self._load_AAXOpenFileWinW()
+	
+	def _load_AAXOpenFileWinW(self):
+		func_name = 'AAXOpenFileWinW'
+		f = self._handle[func_name]
+		f.argtypes = [POINTER(POINTER(c_ubyte)), wintypes.LPWSTR]
+		
+		setattr(self._handle_funcs, func_name, f)
 
-	# def load
+	def AAXOpenFileWinW(self, aax_filename):
+		aax_handle = POINTER(c_ubyte)()
+		print aax_handle
+		fname_buf = wintypes.LPWSTR(aax_filename)
+		print fname_buf
+		
+		returnCode = self._handle_funcs.AAXOpenFileWinW(byref(aax_handle), fname_buf)
+		self._checkFuncResult(returnCode, 'AAXOpenFileWinW')
+		
+		return aax_handle
+	
+	# def AAXCloseFile(self, aax_handle):
+		
