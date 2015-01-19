cd45a039c6520b5b50eb1de634bbf0b636674d79
Shawn <shawn.p.wallis@gmail.com>
2015-01-18 20:40:57 -0500

    - added chapter info data - added audiobook class to manage audiobook - still figuring out how to move through the audio data

--- audible.py
+++ audible.py
@@ -5,15 +5,22 @@ from ctypes import wintypes
 from UserDict import UserDict
 
 
-DEFAULT_DLL_DIRECTORY = "C:\\Program Files (x86)\\Audible\Bin\\AAXSDKWin.dll"
+DEFAULT_DLL_DIRECTORY = "C:\\Program Files (x86)\\Audible\Bin"
 DEFAULT_DLL_FILENAME = "AAXSDKWin.dll"
+
+DEFAULT_AUDIOBOOK_DIRECTORY = "C:\\Users\\Public\\Documents\\Audible\\Downloads"
+
+class DllReturnCodeError(Exception):
+	def __init__(self, returnCode):
+		returnCode = str(returnCode)
+		Exception.__init__(self, returnCode)
 
 class AudibleDll(object):
 	def __init__(self, filepath=None):
 		if filepath is None:
-			self._parentDir = DEFAULT_DLL_DIRECTORY
-			self._filename = DEFAULT_DLL_FILENAME
-			filepath = os.path.join([self._parentDir, self._filename])
+			parentDir = DEFAULT_DLL_DIRECTORY
+			filename = DEFAULT_DLL_FILENAME
+			filepath = os.path.join(parentDir, filename)
 		
 		if os.path.exists(filepath) is False:
 			raise OSError('AAX Library does not exist at \'%s\'' % filepath)
@@ -24,82 +31,245 @@ class AudibleDll(object):
 		self._handle = CDLL(self._filename)
 		
 		self._loadDllFunctions()
-	
-	@staticmethod
-	def _checkFuncResult(returnCode, funcName, raiseError=False):
-		if returnCode != 0:
-			msg = '%s return code was %d' % (funcName, returnCode)
-			if raiseError is True:
-				raise Exception(msg)
-			else:
-				msg = 'WARNING: ' + msg
-				print msg
-	
-	def _loadDllFunctions(self):
-		self._handle_funcs = UserDict()
-		
-		self._load_AAXOpenFileWinW()
-		self._load_AAXCloseFile()
-		self._load_AAXAuthenticateWin()
-		self._load_AAXSeek()
-		self._load_AAXGetAudioChannelCount()
-	
+
 	def _loadDllFunction(self, func_name, func_args):
 		f = self._handle[func_name]
 		f.argtypes = func_args[:]
 		setattr(self._handle_funcs, func_name, f)
-	
-	def _load_AAXOpenFileWinW(self):
+		
+	def _loadDllFunctions(self):
+		self._handle_funcs = UserDict()
+		
 		self._loadDllFunction(
 			'AAXOpenFileWinW',
 			[POINTER(POINTER(c_ubyte)), wintypes.LPWSTR])
-
-	def _load_AAXCloseFile(self):
+			
 		self._loadDllFunction(
 			'AAXCloseFile',
 			[POINTER(c_ubyte)])
-
-	def _load_AAXAuthenticateWin(self):
+			
 		self._loadDllFunction(
 			'AAXAuthenticateWin',
 			[POINTER(c_ubyte)])
-
-	def _load_AAXSeek(self):
+			
 		self._loadDllFunction(
-			'AAXOpenFileWinW',
-			[POINTER(POINTER(c_ubyte)), c_int])
-
-	def _load_AAXGetAudioChannelCount(self):
+			'AAXGetDRMType',
+			[POINTER(c_ubyte), POINTER(c_int)])
+			
+		self._loadDllFunction(
+			'AAXSeek',
+			[POINTER(c_ubyte), c_int])
+			
+		self._loadDllFunction(
+			'AAXSeekToChapter',
+			[POINTER(c_ubyte), c_int])
+			
 		self._loadDllFunction(
 			'AAXGetAudioChannelCount',
-			[POINTER(POINTER(c_ubyte)), POINTER(c_uint)])
-
-	def AAXOpenFileWinW(self, aax_filename):
+			[POINTER(c_ubyte), POINTER(wintypes.DWORD)])
+			
+		self._loadDllFunction(
+			'AAXGetChapterCount',
+			[POINTER(c_ubyte), POINTER(wintypes.DWORD)])
+			
+		self._loadDllFunction(
+			'AAXGetChapterInfo',
+			[POINTER(c_ubyte), c_uint, POINTER(c_char)])
+			
+		self._loadDllFunction(
+			'AAXGetChapterStartTime',
+			[POINTER(c_ubyte), c_uint, POINTER(wintypes.DWORD)])
+			
+		self._loadDllFunction(
+			'AAXGetSampleRate',
+			[POINTER(c_ubyte), POINTER(wintypes.DWORD)])
+			
+		self._loadDllFunction(
+			'AAXGetEncodedAudio',
+			[POINTER(c_ubyte), POINTER(c_char), wintypes.DWORD, POINTER(wintypes.DWORD)])
+	
+	def AAXOpenFileWinW(self, file_path):
 		aax_handle = POINTER(c_ubyte)()
-		fname_buf = wintypes.LPWSTR(aax_filename)
+		fname_buf = wintypes.LPWSTR(file_path)
 		
 		returnCode = self._handle_funcs.AAXOpenFileWinW(byref(aax_handle), fname_buf)
-		self._checkFuncResult(returnCode, 'AAXOpenFileWinW')
-		
-		return (returnCode, aax_handle)
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+		return aax_handle
 	
 	def AAXCloseFile(self, aax_handle):
 		returnCode = self._handle_funcs.AAXCloseFile(aax_handle)
-		self._checkFuncResult(returnCode, 'AAXCloseFile')
-		return (returnCode,)
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
 
 	def AAXAuthenticateWin(self, aax_handle):
 		returnCode = self._handle_funcs.AAXAuthenticateWin(aax_handle)
-		self._checkFuncResult(returnCode, 'AAXAuthenticateWin')
-		return (returnCode,)
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+
+	def AAXGetDRMType(self, aax_handle):
+		drm_type = c_int()
+		
+		returnCode = self._handle_funcs.AAXGetDRMType(aax_handle, byref(drm_type))
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+		return drm_type.value
 
 	def AAXSeek(self, aax_handle, offset):
+		offset = c_int(offset)
 		returnCode = self._handle_funcs.AAXSeek(aax_handle, offset)
-		self._checkFuncResult(returnCode, 'AAXSeek')
-		return (returnCode,)
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+
+	def AAXSeekToChapter(self, aax_handle, chapter):
+		chapter = c_int(chapter)
+		returnCode = self._handle_funcs.AAXSeekToChapter(aax_handle, chapter)
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
 
 	def AAXGetAudioChannelCount(self, aax_handle):
 		channels = wintypes.DWORD()
 		returnCode = self._handle_funcs.AAXGetAudioChannelCount(aax_handle, byref(channels))
-		self._checkFuncResult(returnCode, 'AAXGetAudioChannelCount')
-		return (returnCode,channels)
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+		return channels.value
+
+	def AAXGetChapterCount(self, aax_handle):
+		chapters = wintypes.DWORD()
+		returnCode = self._handle_funcs.AAXGetChapterCount(aax_handle, byref(chapters))
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+		return chapters.value
+
+	def AAXGetChapterInfo(self, aax_handle, chapter):
+		buf_size = 22
+		buf = create_string_buffer(buf_size)
+		# buf = cast(create_string_buffer(buf_size), POINTER(wintypes.BYTE))
+		chapter = c_uint(chapter)
+		
+		returnCode = self._handle_funcs.AAXGetChapterInfo(aax_handle, chapter, buf)
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+		return buf
+
+	def AAXGetChapterMetadata(self, aax_handle, chapter):
+		buf_size = 22
+		buf = create_string_buffer(buf_size)
+		# buf = cast(create_string_buffer(buf_size), POINTER(wintypes.BYTE))
+		chapter = c_uint(chapter)
+		
+		returnCode = self._handle_funcs.AAXGetChapterMetadata(aax_handle, chapter, buf)
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+		return buf
+
+	def AAXGetChapterStartTime(self, aax_handle, chapter):
+		chapter = c_uint(chapter)
+		start_time = wintypes.DWORD()
+		
+		returnCode = self._handle_funcs.AAXGetChapterStartTime(aax_handle, chapter, byref(start_time))
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+		return start_time.value
+	
+	def AAXGetSampleRate(self, aax_handle):
+		sample_rate = wintypes.DWORD()
+		returnCode = self._handle_funcs.AAXGetSampleRate(aax_handle, byref(sample_rate))
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+		return sample_rate.value
+
+	def AAXGetEncodedAudio(self, aax_handle, buf_size):
+		buf = create_string_buffer(buf_size)
+		length = wintypes.DWORD()
+		
+		returnCode = self._handle_funcs.AAXGetEncodedAudio(aax_handle, buf, buf_size, byref(length))
+		if returnCode != 0:
+			raise DllReturnCodeError(returnCode)
+		return (buf, length.value)
+
+class Audiobook:
+	def __init__(self, filepath):
+		if os.path.exists(filepath) is False:
+			filepath = os.path.join(DEFAULT_AUDIOBOOK_DIRECTORY, filepath)
+			print 'Audiobook does not exist at \'%s\'; trying default directory' % filepath
+			if os.path.exists(filepath) is False:
+				raise OSError('Audiobook could not be found')
+
+		self._filepath = filepath
+		
+		self._dll = AudibleDll()
+		self._audiobook_handle = None
+		self.open()
+		self._authenticate()
+		# import struct
+		# for i in range(1,18):
+			# print i
+			# c = self._dll.AAXGetChapterInfo(self._audiobook_handle, i)
+			# print '%08x%08x%08x%08x%08x' % struct.unpack_from('<5I',c.raw),
+			# print '%x%x%x' % struct.unpack_from('<3B', c.raw, offset=20)
+
+	def _verify_opened(self):
+		if self._audiobook_handle is None:
+			raise ValueError('Operation on closed audiobook')
+
+	def _authenticate(self):
+		try:
+			self._dll.AAXAuthenticateWin(self._audiobook_handle)
+		except DllReturnCodeError:
+			raise Exception('Authentication error; trying logging in from AudibleManager first')
+
+	def _get_chapter_offset(self, chapter):
+		offset = 0
+		
+		for i in range(0, (chapter - 1)):
+			offset += self._dll.AAXGetChapterStartTime(self._audiobook_handle, chapter)
+		return offset
+	
+	# def _get_chapter_length(self, chapter):
+		
+	
+	def open(self):
+		if self._audiobook_handle is not None:
+			return
+		self._audiobook_handle = self._dll.AAXOpenFileWinW(self._filepath)
+	
+	def close(self):
+		if self._audiobook_handle is None:
+			return
+		self._dll.AAXCloseFile(self._audiobook_handle)
+		self._audiobook_handle = None
+	
+	def seek(self, offset):
+		self._verify_opened()
+		self._dll.AAXSeek(self._audiobook_handle, offset)
+
+	def get_chapter_count(self):
+		self._verify_opened()
+		chapters = self._dll.AAXGetChapterCount(self._audiobook_handle)
+		return chapters.value
+
+	def get_chapter_encoded_audio(self, book_chapter):
+		chapter_count = self.get_chapter_count()
+		if book_chapter > chapter_count:
+			raise IndexError('Chapter out of bounds (%d chapters)' % chapter_count)
+		# change from 1-indexed (book chapters start at 1) to 0-indexed (arrays start at 0)
+		chapter = book_chapter - 1
+		
+		offset = self._get_chapter_offset(chapter)
+		length = self._dll.AAXGetChapterStartTime(self._audiobook_handle, chapter)
+		print 'off: len'
+		print offset, length
+		
+		self._dll.AAXSeek(self._audiobook_handle, offset)
+		return self._dll.AAXGetEncodedAudio(self._audiobook_handle, length)
+	
+	def dummy(self):
+		self._dll.AAXSeek(self._audiobook_handle, 0)
+		# self._dll.AAXSeekToChapter(self._audiobook_handle, 17)
+		length = 1
+		overall_length = 0
+		while (length > 0):
+			(buf, length) = self._dll.AAXGetEncodedAudio(self._audiobook_handle, 0x400)
+			overall_length += length
+			print '%x,  %x' % (length,overall_length)
--- test.py
+++ test.py
@@ -1,34 +1,51 @@
 
 import audible
 import os
+from binascii import hexlify, unhexlify
 
-dll = audible.AudibleDll("C:\Program Files (x86)\Audible\Bin\AAXSDKWin.dll")
-
-audible_dir = "C:\Users\Public\Documents\Audible\Downloads"
-aax = os.path.join(audible_dir, "OldMansWar_ep6_AH3CF5679N6SR.aax")
-# audible_dir = "C:\Users\shawalli\Downloads"
-# aax = os.path.join(audible_dir, "OldMansWarUnabridged_ep6_AH3CF5679N6SR.aax")
+audible_dir = "C:\Users\shawalli\Downloads"
+aax = os.path.join(audible_dir, "OldMansWarUnabridged_ep6_AH3CF5679N6SR.aax")
 print aax
-print 'Opening File'
-handle = dll.AAXOpenFileWinW(aax)
-print handle
 
-print 'DRM'
-print dll.AAXGetDRMType(handle)
-print 'authenticating'
-print dll.AAXAuthenticateWin(handle)
+# dll = audible.AudibleDll("C:\Program Files (x86)\Audible\Bin\AAXSDKWin.dll")
 
-print 'getting channels'
-print dll.AAXGetAudioChannelCount(handle)
-print 'getting sample rate'
-print dll.AAXGetSampleRate(handle)
-print 'seeking'
-dll.AAXSeek(handle,0)
+# audible_dir = "C:\Users\Public\Documents\Audible\Downloads"
+# aax = os.path.join(audible_dir, "OldMansWar_ep6_AH3CF5679N6SR.aax")
+# print 'Opening File'
+# handle = dll.AAXOpenFileWinW(aax)
+# print handle
 
-print 'getting audio'
-(buf, length) = dll.AAXGetEncodedAudio(handle, 0x400);
-print buf
-print length
+# print 'DRM'
+# print dll.AAXGetDRMType(handle)
+# print 'authenticating'
+# print dll.AAXAuthenticateWin(handle)
 
-dll.AAXCloseFile(handle)
+# print 'getting channels'
+# print dll.AAXGetAudioChannelCount(handle)
+# print 'getting sample rate'
+# print dll.AAXGetSampleRate(handle)
+# print 'seeking'
+# dll.AAXSeek(handle,0)
+# print 'chapters'
+# print dll.AAXGetChapterCount(handle)
+# c = []
+# for i in range(18):
+	# # c.append(dll.AAXGetChapterStartTime(handle, i))
+	# c.append(dll.AAXGetChapterInfo(handle, i).raw)
+# import struct
+# for ch in c:
+	# # print '%08x' % struct.unpack_from('<I', ch)
+	# print '%08x%08x%08x%08x%08x' % struct.unpack_from('<5I', ch),
+	# print '%02x%02x%02x' % struct.unpack_from('<3B', ch, offset=19)
 
+# print 'getting audio'
+# (buf, length) = dll.AAXGetEncodedAudio(handle, 0x400);
+# print buf
+# print length
+
+# dll.AAXCloseFile(handle)
+
+ab = audible.Audiobook(aax)
+ab.dummy()
+# (buf, length) = ab.get_chapter_encoded_audio(1)
+# print length
